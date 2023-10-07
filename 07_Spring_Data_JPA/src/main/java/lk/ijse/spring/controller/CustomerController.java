8ab844b485fe212a3ec57b61c3811717a7f3e4c4
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2023-10-07 20:19:55 +0530

    CustomerController CRUD

--- 07_Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ 07_Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -5,10 +5,11 @@ import lk.ijse.spring.dto.CustomerDTO;
 import lk.ijse.spring.entity.Customer;
 import lk.ijse.spring.repo.CustomerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/customer")
@@ -20,9 +21,41 @@ public class CustomerController {
         System.out.println("CustomerController");
     }
 
+    // get all
+    @GetMapping
+    public List<CustomerDTO> getAllCustomers() {
+        List<Customer> all = repo.findAll();
+        List<CustomerDTO> customerDTOS = new ArrayList<>();
+        for (Customer c : all) {
+            customerDTOS.add(new CustomerDTO(c.getId(), c.getName(), c.getAddress()));
+        }
+        return customerDTOS;
+    }
+
+    // find
+    @GetMapping(params = {"id"})
+    public CustomerDTO findCustomer(String id){
+        Customer c = repo.findById(id).get();
+        return new CustomerDTO(c.getId(),c.getName(),c.getAddress());
+    }
+
+    // add
     @PostMapping
     public void saveCustomer(@ModelAttribute CustomerDTO dto) {
         Customer customer = new Customer(dto.getId(), dto.getName(), dto.getAddress());
         repo.save(customer);
+    }
+
+    // update
+    @PutMapping
+    public void updateCustomer(@RequestBody CustomerDTO dto){
+        Customer customer = new Customer(dto.getId(), dto.getName(), dto.getAddress());
+        repo.save(customer);
+    }
+
+    // delete
+    @DeleteMapping(params = {"id"})
+    public void deleteCustomer(String id){
+        repo.deleteById(id);
     }
 }
