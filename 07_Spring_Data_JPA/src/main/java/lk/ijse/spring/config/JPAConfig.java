5f0b3dfa565caa2fa5823d5a5bb588ec61ec5477
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2023-10-07 19:50:59 +0530

    CustomerRepo added, Save Customer

--- 07_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ 07_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -2,6 +2,7 @@ package lk.ijse.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
@@ -9,28 +10,31 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 @Configuration
+@EnableJpaRepositories(basePackages = "lk.ijse.spring.repo")
+@EnableTransactionManagement
 public class JPAConfig {
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(DataSource ds, JpaVendorAdapter va) {
-        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
-        factoryBean.setPackagesToScan("lk.ijse.spring.entity"); // set entity locations
-        factoryBean.setDataSource(ds);
-        factoryBean.setJpaVendorAdapter(va);
-        return factoryBean;
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter vad) {
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setDataSource(ds);
+        factory.setJpaVendorAdapter(vad);
+        factory.setPackagesToScan("lk.ijse.spring.entity"); // set entity locations
+        return factory;
     }
 
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource ds = new DriverManagerDataSource();
-        ds.setDriverClassName("com.mysql.jdbc.Driver");
-        ds.setUrl("jdbc:mysql://localhost:3306/customerDB?createDatabaseIfNotExist=true");
         ds.setUsername("root");
         ds.setPassword("1234");
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/customerDB?createDatabaseIfNotExist=true");
         return ds;
     }
 
@@ -45,7 +49,7 @@ public class JPAConfig {
     }
 
     @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory factory){
+    public PlatformTransactionManager transactionManager(EntityManagerFactory factory) {
         return new JpaTransactionManager(factory);
     }
 }
--- 07_Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ 07_Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,10 +1,28 @@
 package lk.ijse.spring.controller;
 
+
+import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.entity.Customer;
+import lk.ijse.spring.repo.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/customer")
 public class CustomerController {
+    @Autowired
+    CustomerRepo repo;
+
     public CustomerController() {
         System.out.println("CustomerController");
+    }
+
+    @PostMapping
+    public void saveCustomer(@ModelAttribute CustomerDTO dto) {
+        Customer customer = new Customer(dto.getId(), dto.getName(), dto.getAddress());
+        repo.save(customer);
     }
 }
