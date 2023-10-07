1c459314accc6d9325d00f72b7d369cc6ca98f09
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2023-10-07 17:05:34 +0530

    JPAConfig added

---
+++ 07_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -0,0 +1,51 @@
+package lk.ijse.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+@Configuration
+public class JPAConfig {
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(DataSource ds, JpaVendorAdapter va) {
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setPackagesToScan("lk.ijse.spring.entity"); // set entity locations
+        factoryBean.setDataSource(ds);
+        factoryBean.setJpaVendorAdapter(va);
+        return factoryBean;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/customerDB?createDatabaseIfNotExist=true");
+        ds.setUsername("root");
+        ds.setPassword("1234");
+        return ds;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter va = new HibernateJpaVendorAdapter();
+        va.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect"); // Set the database platform to MySQL 8 Dialect
+        va.setDatabase(Database.MYSQL); // Set the database type to MySQL
+        va.setGenerateDdl(true); // Generate Data Definition Language (DDL) queries
+        va.setShowSql(true); // Show SQL queries in the logs
+        return va;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory factory){
+        return new JpaTransactionManager(factory);
+    }
+}
--- 07_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/WebRootConfig.java
+++ 07_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/WebRootConfig.java
@@ -1,8 +1,10 @@
 package lk.ijse.spring.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import(JPAConfig.class) // Import the JPA configuration class
 public class WebRootConfig {
     public WebRootConfig() {
         System.out.println("WebRootConfig");
